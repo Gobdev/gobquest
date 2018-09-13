43b67b3cf85cea812c6fa114f1f5910539a4ec84
Karutshi <jribom@hotmail.com>
2018-09-13 09:07:16 +0200

    :construction: Add test file for gobreader

---
+++ tests/gobreader/gobreader_test.cpp
@@ -0,0 +1,54 @@
+#include <gtest/gtest.h>
+#include <cstdlib>
+#include <gobreader/linked_list.h>
+#include <gobreader/tokenizer.h>
+
+const string test_dir = string(std::getenv("GOBTEST_DIR")) + "/gobreader/gobfiles/";
+
+TEST (GobreaderTokenTest, CreateToken) {
+    token* a = new token(STRING, "test");
+    EXPECT_EQ(a->str, "test");
+    EXPECT_EQ(a->type, STRING);
+    delete a;
+}
+
+TEST (GoreaderTokenizerTest, CreateTokenizer){
+    tokenizer t(fs::path(test_dir + "simple.gob"));
+    EXPECT_EQ(t.get_current_token().type, START);
+}
+
+TEST (GobreaderLinkedListTest, CreateLinkedList){
+    linked_list<string> a;
+    a.push("test");
+    a.push("test2");
+    a.push("test3");
+    vector<string> results = {"test", "test2", "test3"};
+    int i = 0;
+    for (string b : a){
+        EXPECT_EQ(b, results[i]);
+        i++;
+    }
+    a.pop();
+    a.pop();
+    for (string b : a){
+        EXPECT_EQ(b, "test");
+    }
+}
+
+TEST (GobreaderLinkedListTest, CreateReversedLinkedList){
+    linked_list<string> a;
+    a.push("test");
+    a.push("test2");
+    a.push("test3");
+    vector<string> results = {"test3", "test2", "test"};
+    int i = 0;
+    for (auto b = a.rbegin(); b != a.rend(); b--){
+        EXPECT_EQ(*b, results[i]);
+        i++;
+    }
+    a.pop();
+    a.pop();
+    for (string b : a){
+        EXPECT_EQ(b, "test");
+    }
+}
