d2b12f93e9fa3f330660aed31063d13c2016aabe
jribom <jribom@kth.se>
2019-05-31 14:55:17 +0200

    :bug: Set up necessary global variables in test main

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,6 +1,15 @@
 #include "gtest/gtest.h"
+#include <string>
+#include <controller/debugger/debugger.h>
 
-int main(int argc, char **argv){    
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+// Set up global constants.
+function<void(string)> debug_print_function;
+debug_stream debug;
+
+void f(string s){}
+
+int main(int argc, char **argv){
+    debug_print_function = f;
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
